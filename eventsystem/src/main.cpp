492efd87010e013413020a0400ef5c73177fcb75
pllorente2awesome <pablo.llorente@2awesomestudio.com>
2024-05-18 15:32:55 +0200

    Update main.cpp

--- eventsystem/src/main.cpp
+++ eventsystem/src/main.cpp
@@ -7,8 +7,13 @@ struct LoggingEvent {
 	std::string data;
 };
 
+struct LogErrorEvent {
+	std::string data;
+};
+
 class DataLogger :
-	public EM::IEventListener<LoggingEvent>
+	public EM::IEventListener<LoggingEvent>,
+	public EM::IEventListener<LogErrorEvent>
 {
 private:
 	void debug_data(const std::string& data) {
@@ -18,21 +23,28 @@ public:
 	void OnEvent(const LoggingEvent& ev) override {
 		debug_data(ev.data);
 	}
+
+	void OnEvent(const LogErrorEvent& ev) override {
+		debug_data("Error: " + ev.data);
+	}
 };
 
 int main() {
-	DataLogger* logger = new DataLogger();
+	DataLogger logger;
 
 	LoggingEvent ev;
 	ev.data = "HELLO WORLD!";
 
 	EM::EventSystem::Trigger(ev);
 
-	delete logger;
-
 	ev.data = "WOAAAAH!";
 
 	EM::EventSystem::Trigger(ev);
+
+	LogErrorEvent everror;
+	everror.data = "Unknown :P";
+
+	EM::EventSystem::Trigger(everror);
 
 	return 0;
 }
